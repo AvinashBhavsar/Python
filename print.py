f84b30f4a720d66dbaebb5e4929da6be743b864e
AvinashBhavsar <avinash.bhavsar93@gmail.com>
2026-09-18 13:28:18 +0530

    master commit

--- print.py
+++ print.py
@@ -5,17 +5,9 @@ print(len("hello world"))
 
 str abc:"acv sdjsd"
 
-<<<<<<< HEAD
-print(abc)
-print("abc")
-
-print(xyz) 
-print("abcc")
-
-print("sdf")
-=======
 print(abc123)
 print("abcc")
 
 print("jkl")
->>>>>>> bug-fix
+
+print("tlk")
